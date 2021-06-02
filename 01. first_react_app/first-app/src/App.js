3cd43355701dbf467dabbea1ab6ea409f93c8b1f
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2021-06-02 22:08:54 +0300

    add import and export for two new component

--- 01. first_react_app/first-app/src/App.js
+++ 01. first_react_app/first-app/src/App.js
@@ -1,33 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
-    </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#">Home</a>
-      <a href="#">News</a>
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div className="App">
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>javascript</li>
-        <li>react</li>
-      </ul>
     </div>
   );
 }
---
+++ 01. first_react_app/first-app/src/Technologies.js
@@ -0,0 +1,14 @@
+const Technologies = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>javascript</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
