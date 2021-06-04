286e6331006f22fcc2947c729a37d51e9b3e837f
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2021-06-04 23:59:13 +0300

    create new project SN, components and grid

---
+++ 02.Social_NetworkVer1/social_network/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return <header className="header">
+        Header
+        <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_2.png"></img>
+      </header>
+}
+
+export default Header;
